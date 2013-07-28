6df60ca32741a9267e5a9889d72b7a1bc9e040f1
Tim Becker <tim.becker@kuriositaet.de>
2013-07-28 12:54:19 +0200

    first working!

--- go/statemachine/client.go
+++ go/statemachine/client.go
@@ -1,4 +1,4 @@
-package comms
+package statemachine
 
 // this contains the communication with the main screen ui
 import "jeopardy/json"
@@ -7,23 +7,30 @@ import "code.google.com/p/go.net/websocket"
 
 import (
 	"fmt"
+	"os"
 	J "encoding/json"
 )
 
 type WebsocketHandler struct {
+	conn    *websocket.Conn
 	encoder *J.Encoder
 }
 
 
 func (ws * WebsocketHandler)SetSocket (con *websocket.Conn) {
+	ws.conn = con
 	ws.encoder = J.NewEncoder(con)
 }
 
 func (ws * WebsocketHandler) SendGameState(state *json.GameState) {
+	println(ws.conn)	
 	if err := ws.encoder.Encode(state); err != nil {
-		fmt.Printf("%s", err)
+		fmt.Printf("%s\n", err)
 		panic(err.Error())
 	}
+
+	encoder := J.NewEncoder(os.Stdin)
+	encoder.Encode(state)
 }
 
 
