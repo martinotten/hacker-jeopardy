5182cb9e220a427fc41fadec9df3734f6ce33a04
Martin Otten <martin.otten@adcloud.com>
2013-07-28 12:05:31 +0200

    syntax error fix

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 function renderPlayers(players) {
-  var addPositiveScores(players) {
+  var addPositiveScores = function(players) {
     return players.map(function(player) {
       player['positive_score'] = player['score'] >= 0;
       return player;
@@ -26,7 +26,8 @@ function renderAnswer(answer) {
 $(document).ready(function() {
   var socket = new WebSocket("ws://localhost:9090/ws/");
   socket.onmessage = function (event) {
-    var data = $.parseJSON(event);
+    var data = $.parseJSON(event.data);
+    console.log(event);
     if(data["players"]) {
       renderPlayers(data["players"]);
     }
